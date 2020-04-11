adb651e2070c12eff078b803abb8dde809a75644
Gevio Tabaku <tabakugevio@gmail.com>
2020-04-11 09:04:50 +0200

    second

--- app.js
+++ app.js
@@ -28,13 +28,39 @@ let tempData = [
   {
     type: "Feature",
     properties: {
-      title: "Gramsh",
-      description: "Example description",
-      score: 4.3,
-      imageurl: "https://api.time.com/wp-content/uploads/2017/04/arctic-ocean-garbage-norway.jpg",
+      title: "Trashovica Gramsh",
+      description: "The Trashovica village has a part that is full of garbage because of missing trash cans.",
+      score: 2.4,
+      imageurl: "https://thumbs.dreamstime.com/b/water-pollution-plastic-garbage-polluted-river-filled-plastic-waste-garbage-old-discarded-water-bottles-163011777.jpg",
     },
     geometry: {
       coordinates: [20.17633, 40.872727],
+      type: "Point",
+    },
+  },
+  {
+    type: "Feature",
+    properties: {
+      title: "Parku neighborhood",
+      description: "This place is full of garbage. It needs urget attention to clean it.",
+      score: 1.1,
+      imageurl: "https://api.time.com/wp-content/uploads/2017/04/arctic-ocean-garbage-norway.jpg",
+    },
+    geometry: {
+      coordinates: [20.189839, 40.854595],
+      type: "Point",
+    },
+  },
+   {
+    type: "Feature",
+    properties: {
+      title: "Tirana near the artificial lake",
+      description: "This part of the lake was really polluted but we made a group of volunteers and cleaned it.",
+      score: 5,
+      imageurl: "https://cdn1.i-scmp.com/sites/default/files/styles/768x768/public/images/methode/2018/10/18/06fd8d1e-d05e-11e8-81a4-d952f5356e85_1280x720_184009.JPG",
+    },
+    geometry: {
+      coordinates: [20.189839, 40.854595],
       type: "Point",
     },
   },
